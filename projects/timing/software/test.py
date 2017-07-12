f97f959ba73cf968f5d5d88c5746806c9af8f085
Dave Newbold <dave.newbold@cern.ch>
2017-07-12 15:32:37 +0100

    Adding I2C stuff to test script

--- projects/timing/software/test.py
+++ projects/timing/software/test.py
@@ -6,7 +6,24 @@ import sys
 import collections
 
 uhal.setLogLevelTo(uhal.LogLevel.ERROR)
-board = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.199:50001", "file://addrtab/top.xml")
+hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.199:50001", "file://addrtab/top.xml")
 
-board.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
-board.dispatch()
+hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
+hw.dispatch()
+
+clock_I2C = I2CCore(hw, 10, 5, "i2c", None)
+zeClock=si5344(clock_I2C)
+res= zeClock.getDeviceVersion()
+zeClock.setPage(0, True)
+zeClock.getPage()
+regCfgList=zeClock.parse_clk("Si5344.txt")
+zeClock.writeConfiguration(regCfgList)
+
+hw.getNode("io.freq.ctrl.chan_sel").write(i);
+hw.getNode("io.freq.ctrl.en_crap_mode").write(0);
+hw.dispatch()
+time.sleep(2)
+fq = hw.getNode("io.freq.freq.count").read();
+fv = hw.getNode("io.freq.freq.valid").read();
+hw.dispatch()
+print "Freq:", i, int(fv), int(fq) * 119.20928 / 1000000;
