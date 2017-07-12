e661086df1f052f700d46319f2f49b147b228a8a
Dave Newbold <dave.newbold@cern.ch>
2017-07-12 14:33:10 +0100

    Removing cfg pins

--- projects/timing/firmware/hdl/top_enclustra_ax3_pm3.vhd
+++ projects/timing/firmware/hdl/top_enclustra_ax3_pm3.vhd
@@ -14,7 +14,7 @@ use work.ipbus.ALL;
 entity top is port(
 		sysclk: in std_logic;
 		leds: out std_logic_vector(3 downto 0); -- status LEDs
-		cfg: in std_logic_vector(3 downto 0); -- switches
+--		cfg: in std_logic_vector(3 downto 0); -- switches
 		rgmii_txd: out std_logic_vector(3 downto 0);
 		rgmii_tx_ctl: out std_logic;
 		rgmii_txc: out std_logic;
@@ -92,8 +92,8 @@ begin
 	leds <= not ('0' & userled & inf_leds);
 	phy_rstn <= not phy_rst_e;
 		
-	mac_addr <= X"020ddba1151" & not cfg; -- Careful here, arbitrary addresses do not always work
-	ip_addr <= X"c0a8c81" & not cfg; -- 192.168.200.16+n
+	mac_addr <= X"020ddba1ebc7"; -- Careful here, arbitrary addresses do not always work
+	ip_addr <= X"c0a8ebc7"; -- 192.168.235.199
 
 -- ipbus slaves live in the entity below, and can expose top-level ports
 -- The ipbus fabric is instantiated within.
