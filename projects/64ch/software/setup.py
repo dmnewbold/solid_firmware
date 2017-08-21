dbb57f5e923dca1bc35b2cf75eea03b9085f105a
Dave Newbold <dave.newbold@cern.ch>
2017-08-21 13:10:38 +0100

    Updating ip addr for 64ch test scripts

--- projects/64ch/software/setup.py
+++ projects/64ch/software/setup.py
@@ -7,7 +7,7 @@ from I2CuHal import I2CCore
 from si5344 import si5344
 
 uhal.setLogLevelTo(uhal.LogLevel.ERROR)
-hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.199:50001", "file://addrtab/top.xml")
+hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.50:50001", "file://addrtab/top.xml")
 
 hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
 hw.dispatch()
