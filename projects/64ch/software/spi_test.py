c787329b6ddc4249300878e92ab0c27409cb5083
Dave Newbold <dave.newbold@cern.ch>
2017-08-21 13:34:23 +0100

    Updating script

--- projects/64ch/software/spi_test.py
+++ projects/64ch/software/spi_test.py
@@ -14,11 +14,11 @@ hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.50:50001", "file://addr
 hw.getNode("csr.ctrl.io_sel").write(6) # Talk via CPLD to ADC #3 Bank A SPI
 spi = hw.getNode("io.spi")
 spi.getNode("divider").write(0xf) # Divide 31.25MHz ipbus clock by 32
-spi.getNode("ctrl").write(0x2010) # 16b transfer length, auto CSN
+spi.getNode("ctrl").write(0x2410) # 16b transfer length, auto CSN
 hw.dispatch()
 
 spi.getNode("d0").write(0x04a5) # Write 0xa5 into register 0x4
-spi.getNode("ctrl").write(0x2110) # Do it
+spi.getNode("ctrl").write(0x2510) # Do it
 hw.dispatch()
 
 d = spi.getNode("d0").read()
@@ -27,7 +27,7 @@ hw.dispatch()
 print hex(d), hex(c)
 
 spi.getNode("d0").write(0x8400) # Read from register 0x4
-spi.getNode("ctrl").write(0x2110) # Do it
+spi.getNode("ctrl").write(0x2510) # Do it
 hw.dispatch()
 
 d = spi.getNode("d0").read()
