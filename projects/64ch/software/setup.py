b12b821cfcf39e0a01d37b2008f62a834435b8d4
Dave Newbold <dave.newbold@cern.ch>
2017-08-21 13:10:02 +0100

    Revising 64 setup scripts

--- projects/64ch/software/setup.py
+++ projects/64ch/software/setup.py
@@ -12,6 +12,7 @@ hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.199:50001", "file://add
 hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
 hw.dispatch()
 
+hw.getNode("csr.ctrl.io_sel").write(9) # Talk via CPLD to Si5345
 clock_I2C = I2CCore(hw, 10, 5, "io.i2c", None)
 zeClock=si5344(clock_I2C)
 res= zeClock.getDeviceVersion()
@@ -20,11 +21,11 @@ zeClock.getPage()
 regCfgList=zeClock.parse_clk("Si5345-internal.txt")
 zeClock.writeConfiguration(regCfgList)
 
-hw.getNode("io.freq.ctrl.chan_sel").write(i);
+hw.getNode("io.freq.ctrl.chan_sel").write(0);
 hw.getNode("io.freq.ctrl.en_crap_mode").write(0);
 hw.dispatch()
 time.sleep(2)
 fq = hw.getNode("io.freq.freq.count").read();
 fv = hw.getNode("io.freq.freq.valid").read();
 hw.dispatch()
-print "Freq:", i, int(fv), int(fq) * 119.20928 / 1000000;
+print "Freq:", int(fv), int(fq) * 119.20928 / 1000000;
