2d79133582886f559f2499d6a4c3cd0aaa8a0062
Dave Newbold <dave.newbold@cern.ch>
2017-08-21 15:31:05 +0100

    Updating script

--- projects/64ch/software/spi_test.py
+++ projects/64ch/software/spi_test.py
@@ -3,6 +3,7 @@
 import uhal
 import time
 import sys
+import random
 from I2CuHal import I2CCore
 
 uhal.setLogLevelTo(uhal.LogLevel.ERROR)
@@ -18,20 +19,24 @@ spi.getNode("ctrl").write(0x2410) # 16b transfer length, auto CSN
 spi.getNode("ss").write(0x1) # Enable SPI slave 0
 hw.dispatch()
 
-spi.getNode("d0").write(0x04a5) # Write 0xa5 into register 0x4
-spi.getNode("ctrl").write(0x2510) # Do it
-hw.dispatch()
+for i in range(256):
+	
+	di = random.randint(0x00,0xff)
+	
+	spi.getNode("d0").write(0x0400 + di) # Write 0xa5 into register 0x4
+	spi.getNode("ctrl").write(0x2510) # Do it
+	hw.dispatch()
 
-d = spi.getNode("d0").read()
-c = spi.getNode("ctrl").read()
-hw.dispatch()
-print hex(d), hex(c)
+#	d = spi.getNode("d0").read()
+#	c = spi.getNode("ctrl").read()
+#	hw.dispatch()
+#	print hex(d), hex(c)
 
-spi.getNode("d0").write(0x8400) # Read from register 0x4
-spi.getNode("ctrl").write(0x2510) # Do it
-hw.dispatch()
+	spi.getNode("d0").write(0x8400) # Read from register 0x4
+	spi.getNode("ctrl").write(0x2510) # Do it
+	hw.dispatch()
 
-d = spi.getNode("d0").read()
-c = spi.getNode("ctrl").read()
-hw.dispatch()
-print hex(d), hex(c)
+	d = spi.getNode("d0").read()
+	c = spi.getNode("ctrl").read()
+	hw.dispatch()
+	print hex(di), hex(d & 0xff), hex(c)
