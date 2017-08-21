fd4501ec82e089557d7fe5cb3381361a419f9f0e
Dave Newbold <dave.newbold@cern.ch>
2017-08-21 13:17:54 +0100

    Updating SPI test script

--- projects/64ch/software/spi_test.py
+++ projects/64ch/software/spi_test.py
@@ -8,9 +8,10 @@ from I2CuHal import I2CCore
 uhal.setLogLevelTo(uhal.LogLevel.ERROR)
 hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.50:50001", "file://addrtab/top.xml")
 
-hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
-hw.dispatch()
+#hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
+#hw.dispatch()
 
+hw.getNode("csr.ctrl.io_sel").write(6) # Talk via CPLD to ADC #3 Bank A SPI
 spi = hw.getNode("io.spi")
 spi.getNode("divider").write(0xf) # Divide 31.25MHz ipbus clock by 32
 spi.getNode("ctrl").write(0x10) # 16b transfer length
