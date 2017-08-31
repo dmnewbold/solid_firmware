0716e1a948108f30783a695169d6000df7f62e24
Dave Newbold <dave.newbold@cern.ch>
2017-08-31 17:10:51 +0200

    Adding SI5345 register dump script

---
+++ projects/64ch/software/Si5345_status.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import uhal
+import time
+import sys
+from I2CuHal import I2CCore
+from si5344 import si5344
+
+uhal.setLogLevelTo(uhal.LogLevel.ERROR)
+hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.50:50001", "file://addrtab/top.xml")
+
+hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
+hw.dispatch()
+
+hw.getNode("csr.ctrl.io_sel").write(9) # Talk via CPLD to Si5345
+clock_I2C = I2CCore(hw, 10, 5, "io.i2c", None)
+zeClock=si5344(clock_I2C)
+res= zeClock.getDeviceVersion()
+for ipage in range(10):
+	zeClock.setPage(0, True)
+	for ireg in range(0x100):
+		print hex(ipage), hex(ireg), hex(zeClock.readRegister(ireg, 1))
