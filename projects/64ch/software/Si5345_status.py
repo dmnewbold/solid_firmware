403a9171b0458cf6689d12dcd828cb273e2b419e
David Newbold <phdmn@it064574.users.bris.ac.uk>
2017-08-31 16:55:07 +0100

    BUg fix

--- projects/64ch/software/Si5345_status.py
+++ projects/64ch/software/Si5345_status.py
@@ -7,7 +7,7 @@ from I2CuHal import I2CCore
 from si5344 import si5344
 
 uhal.setLogLevelTo(uhal.LogLevel.ERROR)
-hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.50:50001", "file://addrtab/top.xml")
+hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.55:50001", "file://addrtab/top.xml")
 
 hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
 hw.dispatch()
@@ -17,6 +17,6 @@ clock_I2C = I2CCore(hw, 10, 5, "io.i2c", None)
 zeClock=si5344(clock_I2C)
 res= zeClock.getDeviceVersion()
 for ipage in range(10):
-	zeClock.setPage(0, True)
+	zeClock.setPage(ipage, True)
 	for ireg in range(0x100):
 		print hex(ipage), hex(ireg), hex(zeClock.readRegister(ireg, 1)[0])
