64a4ae52545777c0fa5b8d1d165cec29ce0d3275
Dave Newbold <dave.newbold@cern.ch>
2017-08-31 22:52:27 +0200

    Tidying code

--- projects/64ch/software/setup.py
+++ projects/64ch/software/setup.py
@@ -16,8 +16,6 @@ hw.getNode("csr.ctrl.io_sel").write(9) # Talk via CPLD to Si5345
 clock_I2C = I2CCore(hw, 10, 5, "io.i2c", None)
 zeClock=si5344(clock_I2C)
 res= zeClock.getDeviceVersion()
-zeClock.setPage(0, True)
-zeClock.getPage()
 regCfgList=zeClock.parse_clk("Si5345-internal.txt")
 zeClock.writeConfiguration(regCfgList)
 
