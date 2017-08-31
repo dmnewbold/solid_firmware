e6eebab28a433ea59fb6f0c218f1ece410f5a577
Dave Newbold <dave.newbold@cern.ch>
2017-08-31 23:35:21 +0200

    Tidying code

--- components/solid/software/si5344.py
+++ components/solid/software/si5344.py
@@ -55,8 +55,8 @@ class si5344:
 
     def writeConfiguration(self, regSettingList):
         for item in regSettingList:
-            self.writeRegister(item[0], [item[1]])
+            self.writeRegister(int(item[0]), [int(item[1])])
         for item in regSettingList:
-            d = self.readRegister(item[0])
-            if item[1] != int(d):
+            d = self.readRegister(int(item[0]))
+            if int(item[1]) != int(d):
                 print "Config error", hex(item[0]), hex(item[1]), hex(d)
