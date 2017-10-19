eaafd022bfdca6bf5609ff5441190a1263428c4a
Daniel Saunders <daniel.saunders@cern.ch>
2017-10-19 16:31:33 +0100

    added script to check sync

--- projects/64ch/software/setup.py
+++ projects/64ch/software/setup.py
@@ -7,7 +7,7 @@ from I2CuHal import I2CCore
 from si5344 import si5344
 
 uhal.setLogLevelTo(uhal.LogLevel.INFO)
-hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.86:50001", "file://addrtab/top.xml")
+hw = uhal.getDevice("board", "ipbusudp-2.0://192.168.235.50:50001", "file://addrtab/top.xml")
 
 hw.getNode("csr.ctrl.soft_rst").write(1) # Reset ipbus registers
 hw.dispatch()
@@ -16,7 +16,8 @@ hw.getNode("csr.ctrl.io_sel").write(9) # Talk via CPLD to Si5345
 clock_I2C = I2CCore(hw, 10, 5, "io.i2c", None)
 zeClock=si5344(clock_I2C)
 res= zeClock.getDeviceVersion()
-regCfgList=zeClock.parse_clk("Si5345-internal.txt")
+regCfgList=zeClock.parse_clk("Si5345-RevD-SOL64CZW-SOL64CHW-Registers.txt")
+#regCfgList=zeClock.parse_clk("Si5345-internal.txt")
 zeClock.writeConfiguration(regCfgList)
 
 hw.getNode("io.freq_ctr.ctrl.chan_sel").write(0);
@@ -27,8 +28,9 @@ fq = hw.getNode("io.freq_ctr.freq.count").read();
 fv = hw.getNode("io.freq_ctr.freq.valid").read();
 hw.dispatch()
 print "Freq:", int(fv), int(fq) * 119.20928 / 1000000;
+hw.getNode("daq.timing.csr.ctrl.en_ext_sync").write(1)
 
-'''
+
 f_stat = hw.getNode("csr.stat").read();
 hw.dispatch()
 print "csr.stat", hex(f_stat)
@@ -44,5 +46,4 @@ print "daq.trig.csr.stat", hex(f_ctrl_2)
 # Checks for sync
 fw = hw.getNode("daq.timing.csr.stat.wait_sync").read();
 hw.dispatch()
-#print "wait_sync, sync_err:", int(fw), int(fs)
-'''
+print "wait_sync, sync_err:", int(fw)
