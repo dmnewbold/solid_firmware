dbb2c5c44a7c5bed7b182ab0984fd6f4719a3288
Dave Newbold <dave.newbold@cern.ch>
2018-05-01 16:43:16 +0100

    Fixing ro block length

--- components/solid/firmware/hdl/sc_trig_ro_block.vhd
+++ components/solid/firmware/hdl/sc_trig_ro_block.vhd
@@ -61,7 +61,7 @@ begin
 	keep_c <= (63 downto N_CHAN => '0') & kack when mark = '1' and rising_edge(clk40); 
 
 	go <= (go or (ro_go and keep and not rveto)) and not blkend and trig_en when rising_edge(clk40);
-	blkend <= '1' when ro_ctr = X"06" else '0';
+	blkend <= '1' when ro_ctr = X"04" else '0';
 	ro_valid <= go;
 	ro_blkend <= blkend;
 	
