d1f2a67890b778cdb1f2519e8dc15c516100f852
Dave Newbold <dave.newbold@cern.ch>
2020-12-09 10:38:03 +0000

    Moving to UDP transport for sim

--- boards/pc051a/base_fw/sim/firmware/hdl/pc051a_infra_sim.vhd
+++ boards/pc051a/base_fw/sim/firmware/hdl/pc051a_infra_sim.vhd
@@ -17,8 +17,6 @@ entity pc051a_infra_sim is
 		rst125_o: out std_logic;
 		nuke: in std_logic; -- The signal of doom
 		soft_rst: in std_logic; -- The signal of lesser doom
-		mac_addr: in std_logic_vector(47 downto 0); -- MAC address
-		ip_addr: in std_logic_vector(31 downto 0); -- IP address
 		ipb_in: in ipb_rbus; -- ipbus
 		ipb_out: out ipb_wbus
 	);
@@ -27,73 +25,52 @@ end pc051a_infra_sim;
 
 architecture rtl of pc051a_infra_sim is
 
-	signal clk125_fr, clk125, clk_ipb, clk_ipb_i, rst125, rst_ipb, rst_ipb_ctrl: std_logic;
-	signal mac_tx_data, mac_rx_data: std_logic_vector(7 downto 0);
-	signal mac_tx_valid, mac_tx_last, mac_tx_error, mac_tx_ready, mac_rx_valid, mac_rx_last, mac_rx_error: std_logic;
+	signal clk_ipb, clk_ipb_i, rst, rsti: std_logic;
 	
 begin
 
 --	DCM clock generation for internal bus, ethernet
 
-	clocks: entity work.clock_sim_7s
+	clocks: entity work.clock_sim
 		port map(
-			clko_125 => clk125,
+			clko125 => clk125_o,
 			clko_ipb => clk_ipb_i,
 			locked => open,
 			nuke => nuke,
 			soft_rst => soft_rst,
-			rsto_125 => rst125,
-			rsto_ipb => rst_ipb,
-			rsto_ipb_ctrl => rst_ipb_ctrl
+			rsto => rst,
+			rsto_ctrl => rsti
 		);
 
 	clk_ipb <= clk_ipb_i; -- Best to align delta delays on all clocks for simulation
 	clk_ipb_o <= clk_ipb_i;
-	rst_ipb_o <= rst_ipb;
-	clk125_o <= clk125;
-	rst125_o <= rst125;
+	rst_ipb_o <= rst;
+	rst125_o <= rst;
 	
--- Ethernet MAC core and PHY interface
-	
-	eth: entity work.eth_mac_sim
-		generic map(
-			MULTI_PACKET => true
-		)			
-		port map(
-			clk => clk125,
-			rst => rst125,
-			tx_data => mac_tx_data,
-			tx_valid => mac_tx_valid,
-			tx_last => mac_tx_last,
-			tx_error => mac_tx_error,
-			tx_ready => mac_tx_ready,
-			rx_data => mac_rx_data,
-			rx_valid => mac_rx_valid,
-			rx_last => mac_rx_last,
-			rx_error => mac_rx_error
-		);
-	
--- ipbus control logic
+-- sim UDP transport
 
-	ipbus: entity work.ipbus_ctrl
+	udp: entity work.ipbus_sim_udp
 		port map(
-			mac_clk => clk125,
-			rst_macclk => rst125,
-			ipb_clk => clk_ipb,
-			rst_ipb => rst_ipb_ctrl,
-			mac_rx_data => mac_rx_data,
-			mac_rx_valid => mac_rx_valid,
-			mac_rx_last => mac_rx_last,
-			mac_rx_error => mac_rx_error,
-			mac_tx_data => mac_tx_data,
-			mac_tx_valid => mac_tx_valid,
-			mac_tx_last => mac_tx_last,
-			mac_tx_error => mac_tx_error,
-			mac_tx_ready => mac_tx_ready,
+			clk_ipb => clk_ipb,
+			rst_ipb => rsti,
+			trans_out => trans_in,
+			trans_in => trans_out
+		);
+
+-- IPbus transactor
+
+	trans: entity work.transactor
+		port map (
+			clk => clk_ipb,
+			rst => rsti,
 			ipb_out => ipb_out,
 			ipb_in => ipb_in,
-			mac_addr => mac_addr,
-			ip_addr => ip_addr
-		);
-
+			ipb_req => open,
+			ipb_grant => '1',
+			trans_in => trans_in,
+			trans_out => trans_out,
+			cfg_vector_in => (Others => '0'),
+			cfg_vector_out => open
+		);	
+	
 end rtl;
