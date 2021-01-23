2004ba94603361a63e56857da5e49dc4dc7ac42f
Dave Newbold <dave.newbold@cern.ch>
2021-01-23 19:13:56 +0000

    Moving derand to xpm fifo

--- components/solid/firmware/hdl/sc_derand.vhd
+++ components/solid/firmware/hdl/sc_derand.vhd
@@ -8,8 +8,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
-library unisim;
-use unisim.VComponents.all;
+library xpm;
+use xpm.vcomponents.all;
 
 use work.top_decl.all;
 
@@ -33,57 +33,29 @@ end sc_derand;
 
 architecture rtl of sc_derand is
 	
-	signal rst_ctr: unsigned(3 downto 0);
-	signal rsti, fifo_rst, wen_i: std_logic;
-	signal di, qi: std_logic_vector(63 downto 0);
-	signal dip, qip: std_logic_vector(7 downto 0);
-
 begin
-
-	process(clk_w)
-	begin
-		if rising_edge(clk_w) then
-			if rst_w = '1' then
-				rst_ctr <= "0000";
-			elsif rsti = '1' then
-				rst_ctr <= rst_ctr + 1;
-			end if;
-		end if;
-	end process;
 	
-	rsti <= '0' when rst_ctr = "1111" else '1';
-	fifo_rst <= rsti and rst_ctr(3);
-	wen_i <= wen and not rsti;
-
-	di <= X"00000000" & d;
-	dip <= X"0" & "000" & d_blkend;
-
-	fifo: FIFO36E1
+	fifo: xpm_fifo_async
 		generic map(
-			DATA_WIDTH => 36,
-			FIRST_WORD_FALL_THROUGH => true,
-			ALMOST_FULL_OFFSET => to_bitvector(std_logic_vector(to_unsigned(2 ** (BLK_RADIX - 1) + 8, 16)))
+			FIFO_READ_LATENCY => 0,
+			FIFO_WRITE_DEPTH => DERAND_DEPTH,
+			PROG_FULL_THRESH => 2 ** (BLK_RADIX - 1) + 8,
+			READ_MODE => "fwft"
 		)
 		port map(
-			di => di,
-			dip => dip,
-			do => qi,
-			dop => qip,
+			dout => q,
 			empty => empty,
 			full => full,
-			almostfull => warn,
+			prog_full => warn,
+			din => d,
 			injectdbiterr => '0',
 			injectsbiterr => '0',
-			rdclk => clk_r,
-			rden => ren,
-			regce => '1',
+			rd_clk => clk_r,
+			rd_en => ren,
 			rst => rst_w,
-			rstreg => '0',
-			wrclk => clk_w,
-			wren => wen_i
+			sleep => '0',
+			wr_clk => clk_w,
+			wr_en => wen_i,
 		);
-		
-	q <= qi(31 downto 0);
-	q_blkend <= qip(0);
 
 end rtl;
