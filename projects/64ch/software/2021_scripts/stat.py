2b80e5d8a31aaf010b4fc1de66e984c16291f378
David Newbold dave.newbold@stfc.ac.uk <dmn@heplnw039.pp.rl.ac.uk>
2021-04-10 16:52:54 +0100

    Script updates

--- projects/64ch/software/2021_scripts/stat.py
+++ projects/64ch/software/2021_scripts/stat.py
@@ -18,38 +18,37 @@ v = board.getNode("csr.id").read()
 board.dispatch()
 print("Board ID:", hex(v))
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     chans = int(sys.argv[2])
-else
+else:
     chans = 0
-end if;
 
-board.getNode("csr.stat").read(v)
+v = board.getNode("csr.stat").read()
 board.dispatch()
 print("csr.stat: %x" % (int(v)))
 
-board.getNode("daq.timing.stat").read(v)
+v = board.getNode("daq.timing.csr.stat").read()
 board.dispatch()
-print("daq.timing.stat: %x" % (int(v)))
+print("daq.timing.csr.stat: %x" % (int(v)))
 
-board.getNode("daq.tlink.us_stat").read(v)
-board.getNode("daq.tlink.ds_stat").read(v2)
+v = board.getNode("daq.tlink.us_stat").read()
+v2 = board.getNode("daq.tlink.ds_stat").read()
 board.dispatch()
 print("daq.tlink.us_stat: %x" % (int(v)))
 print("daq.tlink.ds_stat: %x" % (int(2)))
 
-board.getNode("daq.trig.csr.stat").read(v)
-board.getNode("daq.trig.csr.evt_ctr").read(v2)
+v = board.getNode("daq.trig.csr.stat").read()
+v = board.getNode("daq.trig.csr.evt_ctr").read()
 board.dispatch()
 print("daq.trig.csr.stat: %x" % (int(v)))
 print("daq.trig.csr.evt_ctr: %x" % (int(v2)))
 
-board.getNode("daq.roc.csr.stat").read(v)
+v = board.getNode("daq.roc.csr.stat").read()
 board.dispatch()
 print("daq.roc.csr.stat: %x" % (int(v)))
 
 for i in range(chans):
     board.getNode("csr.ctrl.chan").write(i)
-    board.getNode("daq.chan.csr.stat").read(v)
+    v = board.getNode("daq.chan.csr.stat").read()
     board.dispatch()
-    print("chan %x stat: %x" % (int(v)))
+    print("chan %x stat: %x" % (i, int(v)))
